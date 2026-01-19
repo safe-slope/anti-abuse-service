84c9e634e36b92e94332a9bfbd44cecba7552630
Andrej R <andrej.repic04@gmail.com>
2026-01-19 20:53:03 +0100

    feat: Enable JPA repositories and entity scanning for anti-abuse microservice

--- anti-abuse-microservice/api/src/main/java/io/github/safeslope/api/AntiAbuseApplication.java
+++ anti-abuse-microservice/api/src/main/java/io/github/safeslope/api/AntiAbuseApplication.java
@@ -2,8 +2,12 @@ package io.github.safeslope.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "io.github.safeslope")
+@EnableJpaRepositories(basePackages = "io.github.safeslope")
+@EntityScan(basePackages = "io.github.safeslope.entities")
 public class AntiAbuseApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntiAbuseApplication.class, args);
